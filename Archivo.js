76277a66727ea14c8457826545130350a27b6608
DeliaBarron <99303664+DeliaBarron@users.noreply.github.com>
2022-03-08 20:31:42 -0600

    Add files via upload

---
+++ Archivo.js
@@ -0,0 +1,23 @@
+//Multiplicación 
+var numero= Number(prompt("Dame un número"))
+var multiplicacion= numero*2
+console.log(multiplicacion)
+
+//Resta
+var numero1=Number(prompt("Dame el primer número"))
+var numero2=Number(prompt("Dame el segundo número"))
+var resta=numero1-numero2
+console.log(resta)
+
+
+//área de un círculo
+var radio=5.3
+var pi= 3.1416
+var area=pi*radio**2
+console.log(area)
+
+//Área de un Rombo
+var D=8
+var d=6
+var a=(D*d)/2
+console.log(a)
