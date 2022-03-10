8dc87518f3cad941814502f5dac608f694bdf522
Delia Barron <deliabarron.db@gmail.com>
2022-03-09 22:11:33 -0600

    Actividad 3.js

---
+++ Actividad3.js
@@ -0,0 +1,71 @@
+
+// EJERCICIO 1.
+// var numero= Number(prompt("Dame un numero:"))
+// if(numero %2===0){
+//     console.log("El número es divisible entre 2")
+// }else{
+//     console.log("el numero no es divisible entre 2")
+// }
+
+
+// EJERCICIO 2.
+// var numero= Number(prompt("Dame un número:"))
+// if(numero %2===0){
+//     alert("el número es par")
+// }else{
+//     alert("el número es impar")
+// }
+
+
+
+// EJERCICIO 3.
+// var numero=Number(prompt("Dame un número:"))
+// if(numero===1000){
+//         console.log("Ganaste un premio!")
+// }else{
+//     console.log("Tu numero fue: ",numero, "Lo sentimos, sigue participando")
+// }
+
+
+
+
+// EJERCICIO 4
+// var numero1=Number(prompt("Dame el primer número:"))
+// var numero2=Number(prompt("Dame el segundo número:"))
+// if(numero1<numero2){
+//     console.log("El primer número es el menor:",numero1,"<",numero2)
+// }else if(numero2<numero1){
+//     console.log("El segundo número es el menor:",numero2,"<",numero1)
+// }
+
+
+
+
+
+// EJERCICIO 5
+// var numero1=Number(prompt("Dame el primer número:"))
+// var numero2=Number(prompt("Dame el segundo número:"))
+// var numero3=Number(prompt("Dame el tercer número:"))
+
+// if(numero1>numero2 && numero1>numero3){
+//     console.log("El primer número es el mayor:", numero1,">",numero2, "y",numero3)
+
+// }else if(numero1>=numero2 && numero1>numero3){
+//     console.log("el primer y segundo son mayores que el tercero:",numero1 ,"y", numero2 ,">", numero3)
+
+// }else if(numero1>=numero3 && numero1>numero2){
+//     console.log("el primer y tercer numero son mayores que el segundo:",numero1 , "y", numero2, ">", numero2)
+
+// }else if(numero2>numero1 &&numero2>numero3){
+//     console.log("el segundo es el mayor de los tres:", numero2,">", numero1, "y", numero3)
+
+// }else if(numero2>=numero3 &&numero2>numero1){
+//     console.log("el segundo y el tercero son iguales y mayores al primero:", numero2, "y", numero3, "son iguales y > que", numero1)
+
+// }else if(numero3>numero2 && numero3>numero1){
+//     console.log("El tercer número es el mayor:", numero3, ">",numero1, "y", numero2)
+
+// }else {
+//     alert("Te falta este caso!:", "No.1:",numero1, "No.2:",numero2, "No.3:", numero3)
+// }
+
