2d50e5840c91f5482757ca72905c1ac50fa5833a
Delia Barron <99303664+DeliaBarron@users.noreply.github.com>
2022-03-14 17:22:05 -0600

    Add files via upload

---
+++ Actividad5.js
@@ -0,0 +1,16 @@
+// ITERA ARREGLO
+// var arreglo=[1, 4, 6, 10, 22, 55, 46, 2, 5, 0]
+
+
+
+
+//               // ITERAR ARREGLO E IMPRIMIR VALORES MAYORES A 3
+var arreglo=[1, 4, 6, 10, 22, 55, 46, 2, 5, 0]
+
+//2 arreglo y While
+var arreglo=[]
+var index= 0
+while(index<=4){
+    console.log("Elemento No.:", index)
+    index++
+}
